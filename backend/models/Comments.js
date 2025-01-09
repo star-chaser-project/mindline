cdc2bf414166289af4e3ca281c149740417fe5d0
andrewsmoel <andrew.smoel@student.curtin.edu.au>
2025-01-09 16:24:13 +1100

    added Comments schema

---
+++ backend/models/Comments.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const Utils = require('../utils')
+
+// Comment Schema
+const commentSchema = new mongoose.Schema({
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true })
+
+// Middleware
+commentSchema.pre('save', function(next){
+    console.log('document saved')
+    if(this.comment) {
+        this.comment = this.comment.trim();
+    }
+    next();
+})
+
+const commentModel = mongoose.model('Comments', commentSchema)
+
+module.exports = commentModel
