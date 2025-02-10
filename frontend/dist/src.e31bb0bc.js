584ce5fd0ce849a2779c0c62f107646c409e9fec
Gigi <20091877@student.curtin.edu.au>
2025-02-10 18:55:27 +1000

    home

--- frontend/dist/src.e31bb0bc.js
+++ frontend/dist/src.e31bb0bc.js
@@ -16594,7 +16594,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58406" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63728" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
